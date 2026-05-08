33abdb0e44b487776cfbd5b693839c5c3a514836
Brian Wilmeth <bwil@lightbearsoftware.com>
2026-05-08 13:54:33 -0400

    added attributeMenuwidgetController

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,12 @@
+// Copyright LightBear Software
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+}
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+}
