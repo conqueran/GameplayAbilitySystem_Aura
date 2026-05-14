c69742fd18071a6e4c6b18be4d5806b4bd86a77a
Brian Wilmeth <bwil@lightbearsoftware.com>
2026-05-14 13:29:15 -0400

    input tags

---
+++ Source/Aura/Private/Input/AuraInputConfig.cpp
@@ -0,0 +1,23 @@
+// Copyright LightBear Software
+
+
+#include "Input/AuraInputConfig.h"
+
+const UInputAction* UAuraInputConfig::FindAbilityInputActionForTag(const FGameplayTag& InputTag, bool bLogNotFound) const
+{
+	for (const FAuraInputAction& Action : AbilityInputActions)
+	{
+		if (Action.InputAction && Action.InputTag == InputTag)
+		{
+			return Action.InputAction;
+		}
+	}
+
+	if (bLogNotFound)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Can't find AbilityInputAction for InputTag [%s], on Inputconfig [%s]"),
+			*InputTag.ToString(), *GetNameSafe(this))
+	}
+
+	return nullptr;
+}
--- Source/Aura/Public/AbilitySystem/Abilities/AuraGameplayAbility.h
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraGameplayAbility.h
@@ -11,7 +11,7 @@
  */
 UCLASS()
 class AURA_API UAuraGameplayAbility : public UGameplayAbility
-{r
+{
 	GENERATED_BODY()
 	
 };
