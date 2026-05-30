c85bbedb44b414cc1e9391ff897e98035521c34a
Brian Wilmeth <bwil@lightbearsoftware.com>
2026-05-30 13:27:18 -0400

    stuff server in c++

--- Source/Aura/Public/AbilitySystem/AbilityTasks/TargetDataUnderMouse.h
+++ Source/Aura/Public/AbilitySystem/AbilityTasks/TargetDataUnderMouse.h
@@ -6,7 +6,8 @@
 #include "Abilities/Tasks/AbilityTask.h"
 #include "TargetDataUnderMouse.generated.h"
 
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMouseTargetDataSignature, const FVector&, Data);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMouseTargetDataSignature,
+	const FGameplayAbilityTargetDataHandle&, Data);
 
 /**
  * 
@@ -28,4 +29,5 @@ public:
 private:
 
 	virtual void Activate() override;
+	void SendMouseCursorData();
 };
