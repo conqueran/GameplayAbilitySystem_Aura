3b61bf02047dcd0a73e9f7790e2e081c2c31b6ee
Brian Wilmeth <bwil@lightbearsoftware.com>
2026-07-24 19:12:22 -0400

    shaman summon

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraSummonAbility.cpp
@@ -0,0 +1,33 @@
+// Copyright LightBear Software
+
+
+#include "AbilitySystem/Abilities/AuraSummonAbility.h"
+
+TArray<FVector> UAuraSummonAbility::GetSpawnLocations()
+{
+	const FVector Forward = GetAvatarActorFromActorInfo()->GetActorForwardVector();
+	const FVector Location = GetAvatarActorFromActorInfo()->GetActorLocation();
+
+	const float DeltaSpread = ( NumMinions > 1 ) ? SpawnSpread / (NumMinions - 1) : 0.f;
+
+	const FVector LeftOfSpread = Forward.RotateAngleAxis(-SpawnSpread / 2.f, FVector::UpVector);
+	TArray<FVector> SpawnLocations;
+	for (int32 i = 0; i < NumMinions; i++)
+	{
+		const FVector Direction = LeftOfSpread.RotateAngleAxis(DeltaSpread * i, FVector::UpVector);
+		FVector ChosenSpawnLocation = Location + Direction * 
+			FMath::FRandRange(MinSpawnDistance, MaxSpawnDistance);
+
+		FHitResult Hit;
+		GetWorld()->LineTraceSingleByChannel(Hit, ChosenSpawnLocation + FVector(0.f, 0.f, 400.f),
+			ChosenSpawnLocation - FVector(0.f, 0.f, 400.f), ECC_Visibility);
+		if (Hit.bBlockingHit)
+		{
+			ChosenSpawnLocation = Hit.ImpactPoint;
+		}
+		SpawnLocations.Add(ChosenSpawnLocation);
+	}
+
+
+	return SpawnLocations;
+}
