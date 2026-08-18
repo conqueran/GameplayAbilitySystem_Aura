390b6ad9ebfc85713bb18eb4baf31369d5e061c2
Brian Wilmeth <bwil@lightbearsoftware.com>
2026-08-18 14:38:24 -0400

    attributepoints and spell points

--- Source/Aura/Public/UI/WidgetController/AuraWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/AuraWidgetController.h
@@ -6,9 +6,10 @@
 #include "UObject/Object.h"
 #include "AuraWidgetController.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnPlayerStatChangedSignature, int32, NewValue);
+
 class UAttributeSet;
 class UAbilitySystemComponent;
-
 
 USTRUCT(BlueprintType)
 struct FWidgetControllerParams
